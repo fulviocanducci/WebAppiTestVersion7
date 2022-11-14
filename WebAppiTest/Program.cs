67a244b4d8d1e141ed4280928aaf7d61004d9ac7
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2022-11-14 13:00:19 -0300

    Add project files.

---
+++ WebAppiTest/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using WebAppiTest.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MyDataBaseContext>(config =>
+{
+    config.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection"));
+});
+builder.Services.Configure<RouteOptions>(config =>
+{
+    config.LowercaseQueryStrings = true;
+    config.LowercaseUrls = true;
+});
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors(config => config.AllowAnyHeader().AllowAnyOrigin().WithMethods("PUT", "DELETE", "GET", "POST"));
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
